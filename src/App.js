cf024d98eb93d0abf31795d706d76c5235a7590e
jorgeasimas <jorgeasimas@hotmail.com>
2021-06-29 21:13:21 -0700

    update menu-items to navigate withRouter() HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = (props) => (
+  <div>
+    <h1>HATS PAGE</h1>
+    <button type="button" class="btn btn-primary" onClick={() => props.history.push('/')}>Homepage</button>
+  </div>
+)
+
+
 function App() {
   return (
     <div>
-      <HomePage />
+     
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+     
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,7 +13,7 @@ class Directory extends React.Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -50,7 +50,8 @@ class Directory extends React.Component {
             <div className='directory-menu'>
                 {
                     this.state.sections.map(section => 
-                (<MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size}/>
+                (<MenuItem key={section.id} title={section.title} 
+                    imageUrl={section.imageUrl} size={section.size} linkUrl={section.linkUrl}/>
                     ))
                 }
             </div>
