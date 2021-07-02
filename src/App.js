c1107d07e47a8de2f6f51c9a6ff0f2732794ff22
jorgeasimas <jorgeasimas@hotmail.com>
2021-07-01 22:16:38 -0700

    added form input sign in and button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 const HatsPage = (props) => (
@@ -22,6 +23,7 @@ function App() {
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop/hats' component={HatsPage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/sign-in' component={SignInAndSignUpPage}/>
       </switch>
     </div>
   );
